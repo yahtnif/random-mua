2f78f28ebfd4c4d1d115f626f49f16bd6b11baaf
Yahtnif <yahtnif@gmail.com>
2018-12-16 00:52:08 +0800

    Update test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,27 @@
 const randomMua = require('../dist')
 
-it('random-mua', () => {
-  expect(randomMua()).toBeDefined()
-  expect(randomMua('mobile')).toMatch(/mobile/i)
+const TIMES = 100
+
+describe('random-mua', () => {
+  it('desktop', () => {
+    for(let i=0; i< TIMES; i++) {
+      expect(randomMua()).not.toMatch(/mobile/i)
+    }
+  })
+
+  it('mobile', () => {
+    for(let i=0; i< TIMES; i++) {
+      expect(randomMua('mobile')).toMatch(/mobile/i)
+    }
+  })
+
+  // it('unique', () => {
+  //   let prev = randomMua()
+  //   let next
+  //   for(let i=0; i< 50; i++) {
+  //     next = randomMua()
+  //     console.log(i, prev, next)
+  //     expect(i + prev).not.toEqual(i + next)
+  //   }
+  // })
 })
