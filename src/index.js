8e4f95e39949b1bb8ceb2909a3913498b862abc1
Yahtnif <yahtnif@gmail.com>
2018-12-16 00:51:17 +0800

    Random with seed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import ParkMiller from 'park-miller'
 import UA from './ua.json'
 import UAMobile from './ua.mobile.json'
 
-function randomArray(array) {
-  return array[Math.floor(Math.random() * array.length)]
-}
+const RANDOM_MAX = UA.length - 1
+const RANDOM_MOBILE_MAX = UAMobile.length - 1
+const random = new ParkMiller(Math.floor(Math.random() * 100))
 
 export default function(device) {
-  return device ? randomArray(UAMobile) : randomArray(UA)
+  return device === 'mobile' ? UAMobile[random.integerInRange(0, RANDOM_MOBILE_MAX)] : UA[random.integerInRange(0, RANDOM_MAX)]
 }
