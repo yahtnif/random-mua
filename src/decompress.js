4f32d9f9eb75ecb8d7e83ad50e1b87caab2c425f
Yahtnif <yahtnif@gmail.com>
2018-12-16 00:50:34 +0800

    Update decompress file

--- src/decompress.js
+++ src/decompress.js
@@ -1,9 +1,11 @@
-const gunzip = require('gunzip-file')
+const fs = require('fs')
+const { gunzipSync } = require('zlib')
 
 const files = ['ua.json', 'ua.mobile.json']
 
 for (let file of files) {
-  gunzip(`src/${file}.gz`, `src/${file}`, () => {
-    console.log('Decompressed:', file)
-  })
+  let filename = `src/${file}`
+  const compressedData = fs.readFileSync(filename + '.gz')
+  const data = gunzipSync(compressedData)
+  fs.writeFileSync(filename, data)
 }
